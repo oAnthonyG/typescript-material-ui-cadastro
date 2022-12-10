b9a5eae0a297970ae6e7fedb58088828a0466881
Anthony Guilherme <anthonytguilherme@hotmail.com>
2022-12-09 21:08:38 -0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
 
-export function App() {
+export const App = () => {
   return (
-    <div className="App">
-      teste
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
