2b5f1f69d6431f1740e4bf442e5f8b3ce8171759
Anthony Guilherme <anthonytguilherme@hotmail.com>
2023-01-04 17:38:56 -0300

    created a functionality for the search input

--- src/pages/cidades/ListagemDeCidade.tsx
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -1,5 +1,28 @@
+import { useMemo } from "react";
+import { useSearchParams } from "react-router-dom"
+import { FerramentasDaListagem } from "../../shared/components"
+import { LayoutBaseDePagina } from "../../shared/layouts"
+
 export const ListagemDeCidade: React.FC = () =>{
-    return(
-        <div>Listagem de cidades</div>
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const busca = useMemo(()=>{
+        return searchParams.get('busca') || '';
+    }, [searchParams]);
+
+    return( 
+        <LayoutBaseDePagina
+        titulo="Listagem de cidades"
+        barraDeFerramentas={
+            <FerramentasDaListagem
+            mostrarInputBusca
+            textoBotaoNovo="Nova"
+            textoDaBusca={busca}
+            aoMudarTextoDeBusca={texto =>  setSearchParams({busca: texto}, {replace: true})}
+            />
+        }
+        >
+
+        </LayoutBaseDePagina>
     )
 }
